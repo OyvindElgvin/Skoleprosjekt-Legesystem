fe52018f563669e7456071a642bdd146a8b4deee
Øyvind <stoyvind@gmail.com>
2020-03-07 09:25:44 +0100

    starta på metoder for å skrive resepter

--- Lege.java
+++ Lege.java
@@ -21,17 +21,38 @@ class Lege extends UlovligUtskrift implements Comparable<Lege> {
 		return "Lege: "+hentNavn();
 	}
 
-
 	// Henter ut reseptlista til legen
 	public Lenkeliste<Resept> utskrevdeResepter() {
 		return legensReseptListe;
 	}
 
 	public HvitResept skrivHvitResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
-		if (ikkeSpesialist != 0) {
-			//throw new UlovligUtskrift(this, legemiddel);
-		}
+		// LEGG INN UlovligUtskrift HER
 		HvitResept hvitResept = new HvitResept(legemiddel, this, pasient, reit);
 		legensReseptListe.leggTil(hvitResept);
+		return hvitResept;
 	}
+
+	public MillitaerResept skrivMillitaerResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+		// LEGG INN UlovligUtskrift HER
+		MillitaerResept millitaerResept = new MillitaerResept(legemiddel, this, pasient, reit); // sjekk om det er riktig argumenter
+		legensReseptListe.leggTil(millitaerResept);
+		return millitaerResept;
+	}
+
+	public PResept skrivPResept(Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift {
+		// LEGG INN UlovligUtskrift HER
+		PResept pResept = new PResept(legemiddel, this, pasient); // fyll inn her
+		legensReseptListe.leggTil(pResept);
+		return pResept;
+	}
+
+	public BlaaResept skrivBlaaResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+		// LEGG INN UlovligUtskrift HER
+		BlaaResept blaaResept = new BlaaResept(legemiddel, this, pasient); // fyll inn her
+		legensReseptListe.leggTil(blaaResept);
+		return blaaResept;
+	}
+
+
 }
